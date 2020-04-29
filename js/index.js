cde261ee7f7aaceed20eba041fd3ce6b48f25546
davidojes <ojesekho@sheridancollege.ca>
2020-04-29 14:06:38 -0400

    Removed test values and code from master branch.

--- js/index.js
+++ js/index.js
@@ -1,128 +1,3 @@
-// test values to use in place of api
-
-// var countries = [{
-//     "Country": "Namibia",
-//     "CountryCode": "NA",
-//     "Slug": "namibia",
-//     "NewConfirmed": 0,
-//     "TotalConfirmed": 16,
-//     "NewDeaths": 0,
-//     "TotalDeaths": 0,
-//     "NewRecovered": 0,
-//     "TotalRecovered": 8,
-//     "Date": "2020-04-28T17:38:18Z"
-// },
-// {
-//     "Country": "Nauru",
-//     "CountryCode": "NR",
-//     "Slug": "nauru",
-//     "NewConfirmed": 0,
-//     "TotalConfirmed": 0,
-//     "NewDeaths": 0,
-//     "TotalDeaths": 0,
-//     "NewRecovered": 0,
-//     "TotalRecovered": 0,
-//     "Date": "2020-04-28T17:38:18Z"
-// },
-// {
-//     "Country": "Nepal",
-//     "CountryCode": "NP",
-//     "Slug": "nepal",
-//     "NewConfirmed": 0,
-//     "TotalConfirmed": 52,
-//     "NewDeaths": 0,
-//     "TotalDeaths": 0,
-//     "NewRecovered": 0,
-//     "TotalRecovered": 16,
-//     "Date": "2020-04-28T17:38:18Z"
-// },
-// {
-//     "Country": "Netherlands",
-//     "CountryCode": "NL",
-//     "Slug": "netherlands",
-//     "NewConfirmed": 400,
-//     "TotalConfirmed": 38440,
-//     "NewDeaths": 43,
-//     "TotalDeaths": 4534,
-//     "NewRecovered": 0,
-//     "TotalRecovered": 117,
-//     "Date": "2020-04-28T17:38:18Z"
-// },
-// {
-//     "Country": "Netherlands Antilles",
-//     "CountryCode": "AN",
-//     "Slug": "netherlands-antilles",
-//     "NewConfirmed": 0,
-//     "TotalConfirmed": 0,
-//     "NewDeaths": 0,
-//     "TotalDeaths": 0,
-//     "NewRecovered": 0,
-//     "TotalRecovered": 0,
-//     "Date": "2020-04-28T17:38:18Z"
-// },
-// {
-//     "Country": "New Caledonia",
-//     "CountryCode": "NC",
-//     "Slug": "new-caledonia",
-//     "NewConfirmed": 0,
-//     "TotalConfirmed": 0,
-//     "NewDeaths": 0,
-//     "TotalDeaths": 0,
-//     "NewRecovered": 0,
-//     "TotalRecovered": 0,
-//     "Date": "2020-04-28T17:38:18Z"
-// },
-// {
-//     "Country": "New Zealand",
-//     "CountryCode": "NZ",
-//     "Slug": "new-zealand",
-//     "NewConfirmed": 3,
-//     "TotalConfirmed": 1472,
-//     "NewDeaths": 0,
-//     "TotalDeaths": 19,
-//     "NewRecovered": 34,
-//     "TotalRecovered": 1214,
-//     "Date": "2020-04-28T17:38:18Z"
-// },
-// {
-//     "Country": "Nicaragua",
-//     "CountryCode": "NI",
-//     "Slug": "nicaragua",
-//     "NewConfirmed": 0,
-//     "TotalConfirmed": 13,
-//     "NewDeaths": 0,
-//     "TotalDeaths": 3,
-//     "NewRecovered": 0,
-//     "TotalRecovered": 7,
-//     "Date": "2020-04-28T17:38:18Z"
-// }, {
-//     "Country": "Niger",
-//     "NewConfirmed": 43,
-//     "TotalConfirmed": 627,
-//     "NewDeaths": 4,
-//     "TotalDeaths": 18,
-//     "NewRecovered": 20,
-//     "TotalRecovered": 110,
-//     "Date": "2020-04-18T15:08:11Z"
-// },
-// {
-//     "Country": "Nigeria",
-//     "NewConfirmed": 51,
-//     "TotalConfirmed": 493,
-//     "NewDeaths": 4,
-//     "TotalDeaths": 17,
-//     "NewRecovered": 7,
-//     "TotalRecovered": 159,
-//     "Date": "2020-04-18T15:08:11Z"
-// }];
-// var global = {
-//     "NewConfirmed": 83608,
-//     "TotalConfirmed": 2400051,
-//     "NewDeaths": 5534,
-//     "TotalDeaths": 165012,
-//     "NewRecovered": 31584,
-//     "TotalRecovered": 623259
-// }
 
 var countries = [];
 var global = {};
@@ -140,14 +15,7 @@ var globalUpdatesDiv = document.getElementById("globalUpdates");
 var searchResults = [];
 var resultGrid = document.getElementById("resultGrid");
 
-/* code only for testing */
-// globalUpdatesDiv.appendChild(createParagraph("New Confirmed Cases: " + global.NewConfirmed));
-// globalUpdatesDiv.appendChild(createParagraph("Total Confirmed Cases: " + global.TotalConfirmed));
-// globalUpdatesDiv.appendChild(createParagraph("New Deaths: " + global.NewDeaths));
-// globalUpdatesDiv.appendChild(createParagraph("Total Deaths: " + global.TotalDeaths));
-// globalUpdatesDiv.appendChild(createParagraph("New Recoveries: " + global.NewRecovered));
-// globalUpdatesDiv.appendChild(createParagraph("Total Recoveries: " + global.TotalRecovered));
-
+/* request to get current stats */
 var requestURL = 'https://api.covid19api.com/summary';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -232,14 +100,14 @@ function displaySearchResults() {
             var h2 = document.createElement("h2");
             h2.innerHTML = "COVID-19 in " + searchResults[countryIndex].Country;
             resultDiv.appendChild(h2);
-            resultDiv.appendChild(createParagraph("Country Name: " + searchResults[countryIndex].Country));
+            // resultDiv.appendChild(createParagraph("Country Name: " + searchResults[countryIndex].Country));
             resultDiv.appendChild(createParagraph("New Confirmed Cases: " + searchResults[countryIndex].NewConfirmed));
             resultDiv.appendChild(createParagraph("Total Confirmed Cases: " + searchResults[countryIndex].TotalConfirmed));
             resultDiv.appendChild(createParagraph("New Deaths: " + searchResults[countryIndex].NewDeaths));
             resultDiv.appendChild(createParagraph("Total Deaths: " + searchResults[countryIndex].TotalDeaths));
             resultDiv.appendChild(createParagraph("New Recoveries: " + searchResults[countryIndex].NewRecovered));
             resultDiv.appendChild(createParagraph("Total Recoveries: " + searchResults[countryIndex].TotalRecovered));
-            console.log(searchResults[countryIndex].Date);
+            // console.log(searchResults[countryIndex].Date);
             resultDiv.appendChild(createParagraph("Result Timestamp: " + searchResults[countryIndex].Date));
             resultDiv.appendChild(createParagraph("(Note that the date and time are automatically converted to match your local date and time)", "note"));
         })
